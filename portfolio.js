bcbbc083504923a6aa9e03cd8dceeff6801fd808
maria-benmabrouk <om_benmabrouk@esi.dz>
2026-09-07 21:48:43 +0100

    commit1

--- portfolio.js
+++ portfolio.js
@@ -30,26 +30,4 @@ var typed = new Typed("#myText", {
     backDelay: 2000,
     loop: true
 });
-var typed2 = new Typed("#myText2", {
-    strings: [
-        "My portfolio website showcases my skills and projects in web development. Built with HTML, CSS, and JavaScript, it reflects my creativity and passion for creating modern and responsive websites, while continuously evolving with new projects and improvements.",
-        "Click the image to visit the project on GitHub"
-    ],
-    typeSpeed: 40,
-    backSpeed: 10,
-    backDelay: 400,
-    loop: true
 
-});
-
-var typed3 = new Typed("#myText3", {
-    strings: [
-        "The official website of Rawasii provides an overview of the application, download access, and a user guide to help users explore its features and use it effectively. Built using HTML and CSS with a modern and responsive design for a smooth user experience across devices.",
-        "Click the image to visit the project on GitHub"
-    ],
-    typeSpeed: 40,
-    backSpeed: 10,
-    backDelay: 400,
-    loop: true
-
-});
